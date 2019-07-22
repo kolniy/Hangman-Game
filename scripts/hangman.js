a1ada5a455e537641432a67ead33335af492f16e
kolawole olaniyi <kolaniyi3@gmail.com>
2019-07-22 22:57:03 +0100

    init commit

---
+++ scripts/hangman.js
@@ -0,0 +1,149 @@
+class Hangman {
+  constructor(word, remainingGuesses){
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.guessedLetters = []
+    this.status = 'playing'
+  }
+
+  get puzzle(){
+       let puzzle = ''
+       let count = 0
+      this.word.forEach((letter) => {
+       if(this.guessedLetters.includes(letter) || letter === ' '){
+           puzzle += letter
+       } else {
+           puzzle += '*'
+       }
+    });
+    return puzzle
+  }
+
+   makeGuess(guess){
+    if(this.status.toLocaleLowerCase() === 'playing'){
+      guess = guess.toLowerCase()
+      const isUnique = !this.guessedLetters.includes(guess)
+      const isBadGuess = !this.word.includes(guess)
+  
+      if(isUnique){
+          this.guessedLetters.push(guess)
+      }
+  
+      if(isUnique && isBadGuess){
+          this.remainingGuesses--
+      }
+  
+      this.calculateGuesses()
+    }
+   }
+
+   calculateGuesses(){
+     
+  const finished = this.word.every((letter) => this.guessedLetters.includes(letter) || letter === ' ')
+
+    if (this.remainingGuesses === 0){
+    this.status = 'Failed'
+    } else if(finished){
+    this.status = 'Finished'
+      } else {
+    this.status = 'Playing'
+    }
+
+}
+
+get statusMessage() {
+  if(this.status.toLocaleLowerCase() === 'playing'){
+    return `Guesses Left: ${this.remainingGuesses}`
+  } else if(this.status.toLocaleLowerCase() === 'failed'){
+    return `Nice try! The word was "${this.word.join('')}"`
+  } else if(this.status.toLocaleLowerCase() === 'finished'){
+      return `Great Work! You guessed the word`
+  }
+}
+
+}
+
+
+
+
+
+
+
+// const Hangman = function (word, remainingGuesses){
+//     this.word = word.toLowerCase().split('')
+//     this.remainingGuesses = remainingGuesses
+//     this.guessedLetters = []
+//     this.status = 'playing'
+// }
+
+// Hangman.prototype.getPuzzle = function(){
+//     let puzzle = ''
+//     let count = 0
+//   this.word.forEach((letter) => {
+//        if(this.guessedLetters.includes(letter) || letter === ' '){
+//            puzzle += letter
+//        } else {
+//            puzzle += '*'
+//        }
+//     });
+
+//     return puzzle
+// }
+
+// Hangman.prototype.makeGuess = function(guess){
+//   if(this.status.toLocaleLowerCase() === 'playing'){
+//     guess = guess.toLowerCase()
+//     const isUnique = !this.guessedLetters.includes(guess)
+//     const isBadGuess = !this.word.includes(guess)
+
+//     if(isUnique){
+//         this.guessedLetters.push(guess)
+//     }
+
+//     if(isUnique && isBadGuess){
+//         this.remainingGuesses--
+//     }
+
+//     this.calculateGuesses()
+//   }
+  
+// }
+   
+// Hangman.prototype.calculateGuesses = function(){
+
+//   const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
+
+//   if (this.remainingGuesses === 0){
+//     this.status = 'Failed'
+//   } else if(finished){
+//     this.status = 'Finished'
+//   } else {
+//     this.status = 'Playing'
+//   }
+
+// }
+
+// Hangman.prototype.displayStatus = function(){
+//   if(this.status.toLocaleLowerCase() === 'playing'){
+//     return `Guesses Left: ${this.remainingGuesses}`
+//   } else if(this.status.toLocaleLowerCase() === 'failed'){
+//     return `Nice try! The word was "${this.word.join('')}"`
+//   } else if(this.status.toLocaleLowerCase() === 'finished'){
+//       return `Great Work! You guessed the word`
+//   }
+// }
+
+// const lettersUnGuessed = this.word.filter((letter) => {
+    //     return !this.guessedLetters.includes(letter)
+    // })
+
+    // let finished = lettersUnGuessed.length === 0
+    
+    // let finished = true
+    // this.word.forEach((letter) => {
+    //     if(this.guessedLetters.includes(letter)){
+
+    //     } else {
+    //         finished = false
+    //     }
+    // })
---
+++ scripts/request.js
@@ -0,0 +1,34 @@
+const getPuzzle = async (wordCount) => {
+    const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    if(response.status === 200){
+        const data = await response.json()
+        return data.puzzle
+    } else {
+        throw new error('Unable to get puzzle')
+    }   
+}
+
+const getCountryDetails = async (countryCode) => {
+    const response = await fetch('//restcountries.eu/rest/v2/all')
+    if(response.status = 200){
+    const countries = await response.json()
+    return countries.find((country) => country.alpha2Code === countryCode)
+    } else {
+        throw new error('cannot fetch country details')
+    }
+}
+
+const getLocation = async () => {
+   const response = await fetch('//ipinfo.io/json?token=f452f11cb3f090')
+   if(response.status === 200){
+       const location = await response.json()
+       return location
+   } else {
+       throw new Error('Cannot fetch location details')
+   }
+}
+
+const getCurrentCountry = async () => {
+  const location = await getLocation()
+   return getCountryDetails(location.country)
+}
